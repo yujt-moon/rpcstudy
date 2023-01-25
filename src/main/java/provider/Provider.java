723e745554263c1977c08d32893fee156cd56b26
yujt-moon <1291932524@qq.com>
2023-01-25 21:37:52 +0800

    部分提交

--- src/main/java/provider/Provider.java
+++ src/main/java/provider/Provider.java
@@ -1,9 +1,14 @@
 package provider;
 
+import org.apache.log4j.Logger;
 import protocol.http.HttpServer;
 
 public class Provider {
+
+    private static Logger logger = Logger.getLogger(Provider.class);
+
     public static void main(String[] args) {
+        logger.warn("启动应用！");
         // 1. 本地注册
 
         // 2. 远程注册
